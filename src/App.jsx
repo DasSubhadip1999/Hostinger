9b33d768af2ab25ed0fca4e4fe159dc6dcc4095d
DasSubhadip1999 <subhadipdasrng54@gmail.com>
2022-09-17 00:42:09 +0530

    domain search add

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./Components/Header"
 import Services from "./Components/Services/Services";
 import {ResponsiveProvider} from "./Context/ResponsiveContext";
 import OurServices from "./Components/OurServicesInclude/OurServices";
+import DomainName from "./Components/DomainName/DomainName";
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
           <Header />
           <Services />
           <OurServices />
+          <DomainName />
       </div>
     </ResponsiveProvider>
   )
---
+++ src/Components/DomainName/DomainName.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './domainname.css'
+
+function DomainName() {
+  return (
+    <div>DomainName</div>
+  )
+}
+
+export default DomainName
